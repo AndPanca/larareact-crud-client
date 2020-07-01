fd2ada8f8cd5e8a4794572cc5fd0bd7913f78203
AndPanca <andripanca11@gmail.com>
2020-07-01 23:41:01 +0700

    comment App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 function App() {
     return (
         <>
+        {/* Ini untuk posisi component paling atas */}
         <CssBaseline />
             <Home />
         </>    
