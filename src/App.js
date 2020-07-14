c1cadd3ab67ed0033ca0cb6652e1e6ff9f20c60a
AndPanca <andripanca11@gmail.com>
2020-07-14 21:36:03 +0700

    Render home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import Home from "./components";
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-function App() {
-    return (
-        <>
-        {/* Ini untuk posisi component paling atas */}
-        <CssBaseline />
-            <Home />
-        </>    
-    );
+class App extends Component{
+    render() {
+        return (
+            <>
+            {/* Ini untuk posisi component paling atas */}
+            <CssBaseline />
+                {/* Buka Component Home dari Index.jd di Folder Component */}
+                <Home />
+            </>    
+        );
+    }
 }
 
 export default App;
