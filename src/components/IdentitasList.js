c616946c78876c7f3ed70b18513c38e7969a0905
AndPanca <andripanca11@gmail.com>
2020-07-14 21:38:07 +0700

    Menampilkan isi tabel identitas dr API

--- src/components/IdentitasList.js
+++ src/components/IdentitasList.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Identitas } from './Identitas';
+import React, { Component } from 'react';
+import Identitas from './Identitas';
 import { withStyles } from '@material-ui/core/styles';
 import { Container, Table, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
 
@@ -13,38 +13,58 @@ const StyledTableCell = withStyles((theme) => ({
     },
 }))(TableCell);
 
-const StyledTableRow = withStyles((theme) => ({
-    root: {
-        '&:nth-of-type(odd)': {
-            backgroundColor: theme.palette.action.hover,
-        },
-    },
-}))(TableRow);
+class IdentitasList extends Component {
+    onEdit = (data) => {
+        this.props.onEdit(data);
+        // console.log("identitas list", data);
+    };
 
-const IdentitasList = () => {
-    return (
-        <div>
-            <Container>
-                <TableContainer component={Paper}>
-                    <Table aria-label="customized table">
-                        <TableHead>
-                            <TableRow>
-                                <StyledTableCell>Id</StyledTableCell>
-                                <StyledTableCell>Nama</StyledTableCell>
-                                <StyledTableCell align="right">Birth Year</StyledTableCell>
-                                <StyledTableCell>Gender</StyledTableCell>
-                                <StyledTableCell>Email</StyledTableCell>
-                                <StyledTableCell align="right">Phone</StyledTableCell>
-                                <StyledTableCell>Address</StyledTableCell>
-                                <StyledTableCell>Action</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <Identitas />
-                    </Table>
-                </TableContainer>
-            </Container>
-        </div>
-    )
+    // Menangkap PROPS ID yang akan di DELETE
+    onDelete = (id) => {
+        this.props.onDelete(id);
+        // console.log("delete id", id);
+    };
+
+    render() {
+        // Menangkap data state dari index.js dengan PROPS dan disimpan di const identitas.
+        const identitas = this.props.identitas;
+        return (
+            <div>
+                <Container>
+                    <TableContainer component={Paper}>
+                        <Table aria-label="customized table">
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell>Id</StyledTableCell>
+                                    <StyledTableCell>Nama</StyledTableCell>
+                                    <StyledTableCell>Birth Year</StyledTableCell>
+                                    <StyledTableCell>Gender</StyledTableCell>
+                                    <StyledTableCell>Email</StyledTableCell>
+                                    <StyledTableCell>Phone</StyledTableCell>
+                                    <StyledTableCell>Address</StyledTableCell>
+                                    <StyledTableCell>Action</StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+                            {/* Melempar lago data identitas ke Identitas.js, dengan maping */}
+                            {
+                                identitas.map(identitas =>{
+                                    return (
+                                        <Identitas 
+                                            identitas={identitas}
+                                            key={identitas.id}
+                                            // Melempar ID yang akan di DELETE ke Index.js 
+                                            onDelete={this.onDelete}
+                                            onEdit={this.onEdit}
+                                        />
+                                    );
+                                })
+                            }
+                        </Table>
+                    </TableContainer>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default IdentitasList
