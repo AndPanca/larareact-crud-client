f58d1f048c0baad59f9acc7cb4550b8c77377c0e
AndPanca <andripanca11@gmail.com>
2020-07-14 21:36:45 +0700

    Perbaiki form concept

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,5 @@
 import 'date-fns';
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import React, {Component} from 'react';
 import { Container, Grid, TextField, Button } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
 import DateFnsUtils from '@date-io/date-fns';
@@ -10,9 +9,10 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import { withStyles } from "@material-ui/core/styles";
 
-
-const useStyles = makeStyles((theme) => ({
+//CSS INLINE
+const useStyles = theme => ({
     root: {
         width: "95%",
         marginBottom: theme.spacing(1)
@@ -22,76 +22,208 @@ const useStyles = makeStyles((theme) => ({
         marginBottom: theme.spacing(3),
         textAlign: 'center'
     }
-}));
+});
 
-const Form = () => {
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2000-01-01T21:11:54'));
-    const handleDateChange = (date) => {
-        setSelectedDate(date);
+class Form extends Component {
+    //SET default State form dan button
+    state = {
+        form: {id: "", name: "", birthday: new Date('01-01-2003'), gender: "M", email: "", phone: "", address: "", isEdit: false},
+        btnName: "Submit",
+        btnColor: "primary",
     };
-    const [value, setValue] = React.useState('female');
 
-    const handleChange = (event) => {
-        setValue(event.target.value);
+    //Cek data kosong
+    isEmpty(obj){
+        return Object.entries(obj).length === 0 && obj.constructor === Object;
     };
-    const classes = useStyles();
-    return (
-        <div>
-            <Container>
-                <Typography variant="h5" className={classes.judul}>Formulir Identitas</Typography>
-                <form noValidate autoComplete="off">
-                    <Grid container direction="row" justify="flex-start" alignItems="flex-start" spacing={1}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={6}>
-                                <TextField id="standard-basic" className={classes.root} label="Id" placeholder="NIK/KTM" />
+
+    //Update button dan form setelah pilih edit
+    componentDidUpdate(prevProps){
+        if (prevProps !== this.props && !this.isEmpty(this.props.identitasId)){
+          this.setState({
+            form: {...this.props.identitasId, isEdit: true},
+            btnName: "Update",
+            btnColor: "secondary"
+          });
+        }
+    };
+
+    //handle data tanggal
+    handleDateChange = date => {
+        this.setState({
+          form: {
+            ...this.state.form,
+            birthday: date
+          }
+        })
+    };
+
+    //handle data radio
+    handleGenderChange = (e) => {
+        this.setState({
+          form: {
+            ...this.state.form,
+            gender: e.target.value
+          }
+        })
+    };
+
+    //handle data setiap input
+    handleChange = event =>{
+      const {name, value} = event.target;
+      let form = this.state.form;
+      form[name] = value;
+      this.setState({form});
+    };
+
+    onFormSubmit = event => {
+      //preven form submit
+      event.preventDefault();
+      //Cek Validasi form
+      if (this.formValidation()){
+        //Kirim data props onFormSubmit ke Index.js 
+        this.props.onFormSubmit(this.state.form);
+      }
+      //Membersihkan form setelah update
+      this.clearFormFields();
+    };
+
+    formValidation = () => {
+      //Id
+      if(document.getElementsByName("id")[0].value === ""){
+        alert('Enter Id');
+        return false;
+      }
+      //Name
+      if(document.getElementsByName("name")[0].value === ""){
+        alert('Enter Name');
+        return false;
+      }
+      //Email
+      if(document.getElementsByName("email")[0].value === ""){
+        alert('Enter Email');
+        return false;
+      }
+      //Phone
+      if(document.getElementsByName("phone")[0].value === ""){
+        alert('Enter Phone');
+        return false;
+      }
+      //Address
+      if(document.getElementsByName("address")[0].value === ""){
+        alert('Enter Address');
+        return false;
+      }
+      return true;
+    };
+
+    clearFormFields = () => {
+      //Membersihkan form input setalah submit dan update
+      this.setState({
+        form: {id: "", name: "", birthday: new Date('01-01-2003'), gender: "M", email: "", phone: "", address: "", isEdit: false}
+      })
+    };
+    
+    render () {
+        const { classes } = this.props;
+        return (
+            <div>
+                <Container>
+                    <Typography variant="h5" className={classes.judul}>Formulir Identitas</Typography>
+                    <form noValidate autoComplete="off">
+                        <Grid container direction="row" justify="flex-start" alignItems="flex-start" spacing={1}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={6}>
+                                    <TextField 
+                                      id="standard-basic" 
+                                      className={classes.root}
+                                      name="id" 
+                                      label="Id" 
+                                      placeholder="NIK/KTM"
+                                      value={this.state.form.id}
+                                      onChange={this.handleChange}/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField 
+                                      id="standard-basic" 
+                                      className={classes.root} 
+                                      name="name"
+                                      label="Name" 
+                                      placeholder="Full name"
+                                      value={this.state.form.name}
+                                      onChange={this.handleChange}/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <KeyboardDatePicker
+                                            disableToolbar
+                                            variant="inline"
+                                            margin="normal"
+                                            format="yyyy-MM-dd"
+                                            id="date-picker-inline"
+                                            label="Birthday"
+                                            className={classes.root}
+                                            value={this.state.form.birthday}
+                                            onChange={this.handleDateChange}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change date',
+                                            }}
+                                        />
+                                    </MuiPickersUtilsProvider>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <FormControl style={{ marginTop: '2ch' }} component="fieldset">
+                                        <FormLabel component="legend">Gender</FormLabel>
+                                        <RadioGroup row aria-label="gender" name="gender" value={this.state.form.gender} onChange={this.handleGenderChange}>
+                                            <FormControlLabel value="F" control={<Radio />} label="Female" />
+                                            <FormControlLabel value="M" control={<Radio />} label="Male" />
+                                        </RadioGroup>
+                                    </FormControl>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField 
+                                      id="standard-basic" 
+                                      className={classes.root} 
+                                      name="email"
+                                      label="Email" 
+                                      placeholder="blabla@mail.com"
+                                      value={this.state.form.email}
+                                      onChange={this.handleChange} />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField 
+                                      id="standard-basic" 
+                                      className={classes.root} 
+                                      name="phone"
+                                      label="Phone Number" 
+                                      placeholder="+62"
+                                      value={this.state.form.phone}
+                                      onChange={this.handleChange}/>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <TextField 
+                                      id="standard-multiline-static" 
+                                      className={classes.root} 
+                                      name="address"
+                                      label="Address" 
+                                      multiline rows={2}
+                                      value={this.state.form.address}
+                                      onChange={this.handleChange}/>
+                                    <Button 
+                                      variant="contained" 
+                                      style={{marginTop: '4ch', marginBottom:'4ch'}} 
+                                      color={this.state.btnColor} 
+                                      onClick={this.onFormSubmit}>
+                                      {this.state.btnName}
+                                    </Button>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={6}>
-                                <TextField id="standard-basic" className={classes.root} label="Name" placeholder="Full name" />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                    <KeyboardDatePicker
-                                        disableToolbar
-                                        variant="inline"
-                                        format="dd/MM/yyyy"
-                                        margin="normal"
-                                        id="date-picker-inline"
-                                        label="Birthday"
-                                        className={classes.root}
-                                        value={selectedDate}
-                                        onChange={handleDateChange}
-                                        KeyboardButtonProps={{
-                                            'aria-label': 'change date',
-                                        }}
-                                    />
-                                </MuiPickersUtilsProvider>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <FormControl style={{ marginTop: '2ch' }} component="fieldset">
-                                    <FormLabel component="legend">Gender</FormLabel>
-                                    <RadioGroup row aria-label="gender" name="gender" value={value} onChange={handleChange}>
-                                        <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                        <FormControlLabel value="male" control={<Radio />} label="Male" />
-                                    </RadioGroup>
-                                </FormControl>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField id="standard-basic" className={classes.root} label="Email" placeholder="blabla@mail.com" />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField id="standard-basic" className={classes.root} label="Phone Number" placeholder="+62" />
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField id="standard-multiline-static" className={classes.root} label="Address" multiline rows={2} />
-                                <Button variant="contained" style={{marginTop: '4ch', marginBottom:'4ch'}} color="primary" href="#contained-buttons">Submit</Button>
-                            </Grid>
-                            
                         </Grid>
-                    </Grid>
-                </form>
-            </Container>
-        </div>
-    )
+                    </form>
+                </Container>
+            </div>
+        )
+    }
 }
-
-export default Form;
+// Untuk menggunakan Style inline, convert dari hooks ke class
+export default withStyles(useStyles)(Form);
